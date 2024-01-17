34c0e79e8979a1083d17072db395ad23f5dd88ea
wilsonevs1988 <wilsonev.saldarriaga@hotmail.com>
2024-01-16 23:48:21 -0500

    Ultimo Commit

--- subo.bat
+++ subo.bat
@@ -3,6 +3,6 @@ git commit -m "Ultimo Commit"
 git push
 set GOOS=linux
 set GOARCH=amd64
-go build ./src/ main.go
-del ./src/main.zip
+go build ./src/main.go
+del main.zip
 tar.exe -a -cf main.zip main
